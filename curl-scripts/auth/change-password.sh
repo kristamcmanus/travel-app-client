db6e4ef023df1bffb4b03b85fef0291c3145bd43
kristamcmanus <k.mcmanus310@gmail.com>
2021-06-02 12:26:18 -0400

    Created curl-scripts and added sign-up/in and change password forms

---
+++ curl-scripts/auth/change-password.sh
@@ -0,0 +1,13 @@
+curl "http://localhost:4741/posts" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+    }
+  }'
+
+echo
