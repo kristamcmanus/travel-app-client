ba371eccacc009eab2a7c8e2f5b1507944fe2253
kristamcmanus <k.mcmanus310@gmail.com>
2021-06-07 01:02:03 -0400

    Fix typo in production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://cryptic-ocean-71139.herokuapp.com/',
+  production: 'https://cryptic-ocean-71139.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
